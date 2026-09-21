8718fc146d5dee8756cba41e6f42a365cc4820e4
Payton <hitotsuchance@gmail.com>
2026-09-21 02:30:30 -0500

    Just added a a comment into main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+/*Wanted to try making a CPU emulator after finding out about it online. We'll see how it goes.
+I have no idea what I'm doing, but I guess that's the point of this project. 9/21/26
+*/
+
 #include <iostream>
 
 using namespace std;
